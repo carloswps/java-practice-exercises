25a01afcc89b20606d66127364ff3ec838d6fc35
carloswps <carloswendell62@gmail.com>
2025-03-03 21:44:18 -0300

    first commit

---
+++ src/ImpostoSalario.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ImpostoSalario {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor do seu Salario: ");
+        float salario = scanner.nextFloat();
+        System.out.println("Digite o valor dos seus Beneficios: ");
+        float valorBeneficios = scanner.nextFloat();
+
+        float valorImposto = 0;
+        if (salario >= 0.00 && salario <= 1100.00) {
+            valorImposto = 0.05F * salario;
+        }else if (salario >= 1100.01 && salario <= 2500.00) {
+            valorImposto = 0.10F * salario;
+        }else{
+            valorImposto = 0.15F * salario;
+        }
+
+        float saida = (salario - valorImposto) + valorBeneficios;
+        System.out.println(String.format("%.2f", saida));
+
+        scanner.close();
+    }
+}
---
+++ src/SimulacaoBancaria.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+        
+        while (continuar) {
+            System.out.println("Digite a opção desejada \n");
+            System.out.println("1. Depositar");
+            System.out.println("2. Sacar");
+            System.out.println("3. Consutar Saldo");
+            System.out.println("4. Encerrar ");
+            int opcao = scanner.nextInt();
+            switch (opcao) {
+            case 1:
+                System.out.println("Digite o valor a ser Depositado \n");
+                Double valorDeposito = scanner.nextDouble();
+                if (valorDeposito > 0) {
+                    saldo += valorDeposito;
+                }else {
+                    System.out.println("Valor digitado invalido");
+                };
+                System.out.println("Saldo atual: "+ saldo );
+                break;
+            case 2:
+                System.out.println("Digite o valor solicitado para saque ");
+                double valorSaque = scanner.nextDouble();
+                if (valorSaque <= saldo) {
+                    saldo -= valorSaque;
+                    System.out.println("Seu saldo atual é de: "+saldo);
+                    
+                }else {
+                    System.out.println("Valor indisponivel");
+                }
+                    break;
+                case 3:
+                System.out.println("O valor atual disponivel é de: " + saldo);
+                    break;
+                case 4:
+                    System.out.println("Programa encerrado.");
+                    continuar = false; // Atualiza a variável de controle para encerrar o loop
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
